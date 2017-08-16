b090a3e622bc9d24cbbec944f7d4a9269733ae23
Pieter Provoost <pieterprovoost@gmail.com>
2017-08-16 10:13:02 +0200

    cleanup tests

--- test/test.py
+++ test/test.py
@@ -7,9 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 class TestDwCAProcessor(unittest.TestCase):
 
     def testRead(self):
-
-        archive = DwCAProcessor("data/dwca-north_sea_hypbent_com-v1.9.zip")
-        print archive
+        pass
 
 if __name__ == "__main__":
     unittest.main()
