bb0a291adc8d9de93bd06b3555cdf1725406b20f
Pieter Provoost <pieterprovoost@gmail.com>
2017-11-03 14:31:05 +0100

    set up testing

--- test/dev.py
+++ test/dev.py
@@ -4,22 +4,16 @@ import json
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 from dwcaprocessor import DwCAProcessor
 
-
-archive = DwCAProcessor("data/event_occurrence_emof.zip")
+archive = DwCAProcessor("./data/archives/dwca-nsbs-v1.6.zip")
 print archive
 
-count = 0
 for coreRecord in archive.coreRecords():
-    count += 1
-    if count > 20: exit()
     print "+++ core: " + archive.core.type
     print json.dumps(coreRecord, indent=2)
     for e in archive.extensions:
         print "--- extension: " + e.type
         for extensionRecord in archive.extensionRecords(e):
             print json.dumps(extensionRecord, indent=2)
-
-
 
 #for e in archive.extensions:
 #    print e.type
--- test/test.py
+++ test/test.py
@@ -1,13 +1,24 @@
 import unittest
 import logging
+import sys
+import os
+import json
 from dwcaprocessor import DwCAProcessor
 
-logging.basicConfig(level=logging.DEBUG)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+logging.basicConfig(level=logging.INFO)
 
 class TestDwCAProcessor(unittest.TestCase):
 
-    def testRead(self):
-        pass
+    def testNSBS(self):
+        archive = DwCAProcessor("./data/archives/dwca-nsbs-v1.6.zip")
+        for coreRecord in archive.coreRecords():
+            if coreRecord["source"]["eventID"] == "Cruise1:Station783:Grabbiotic:980_1":
+                self.assertTrue("eventDate" not in coreRecord["source"])
+                self.assertTrue("eventDate" in coreRecord["full"])
+                self.assertTrue("decimalLatitude" not in coreRecord["source"])
+                self.assertTrue("decimalLatitude" in coreRecord["full"])
+                break
 
 if __name__ == "__main__":
     unittest.main()
